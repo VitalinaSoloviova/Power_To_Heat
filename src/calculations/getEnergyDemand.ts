11397e3b654a5d60328f263d8b044776ed002cc0
Vitalina Soloviova <161602935+VitalinaSoloviova@users.noreply.github.com>
2026-04-23 06:50:28 +0200

    Add city data model and energy demand calculation functions

---
+++ src/calculations/getEnergyDemand.ts
@@ -0,0 +1,18 @@
+import { type CityProfile, city } from "./CityData"
+
+// returns estimated hourly demand in kilowatts
+// param: outside temperature, city (as for now city is "our" city with hardcoded values)
+//NB! if outsideTemp >= targetInsideTemp -> energy demand = 0
+
+export function getEenergyDemand(outsideTemp: number, city: CityProfile) {
+    return Math.max(
+        0,              // returns 0 if result < 0 i.e. outsideTemp > insideTemp
+        city.clients * 
+        city.energyDemandPerPerson * 
+        (city.targetInsideTemp - outsideTemp) // for MW => / 1000
+    )
+}
+
+const outsideTemp: number = -10
+const demand = getEenergyDemand(outsideTemp, city)
+console.log(demand)
---
+++ src/calculations/getPriceData.ts
@@ -0,0 +1,5 @@
+/*
+this file contains functions to retrieve data from the database 
+based on the paramaters passed from the UI
+params: periodStart, periodEnd
+*/
