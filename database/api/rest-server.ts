25d2efe0b7aa5500abc6d6b8ccec803fb6190514
Amelie Plomer <amelie.plomer@stud.h-da.de>
2026-04-28 11:43:47 +0200

    bug fix

--- database/api/rest-server.ts
+++ database/api/rest-server.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import type { Request, Response } from 'express';
 import pg from 'pg';
 
 const { Pool } = pg;
