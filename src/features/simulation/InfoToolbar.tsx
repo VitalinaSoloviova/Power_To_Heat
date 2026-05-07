2659b88aa6a7fb7fa026988ad81b43d42eae8ad8
Vitalina Soloviova <161602935+VitalinaSoloviova@users.noreply.github.com>
2026-05-07 20:53:41 +0200

    update InfoToolbar width from 900px to 750px

--- src/features/simulation/InfoToolbar.tsx
+++ src/features/simulation/InfoToolbar.tsx
@@ -25,7 +25,7 @@ const InfoToolbar: React.FC<InfoToolbarProps> = ({ point }) => {
           0 4px 12px rgba(0,0,0,0.25),
           inset 0 1px 0 rgba(255,255,255,0.1)
         `,
-        width: '900px',
+        width: '750px',
       }}
     >
 
