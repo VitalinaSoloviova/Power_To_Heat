d5d9129a4fa1768de773d14582b845ea34d574b9
Amelie Plomer <amelie.plomer@stud.h-da.de>
2026-05-12 13:50:47 +0200

    fix cicd

--- src/services/ui/SimulationUIService.ts
+++ src/services/ui/SimulationUIService.ts
@@ -1,49 +1,36 @@
+// TODO: this service is currently unused and has been commented out
+// because CityDemandResolver was refactored (resolve() → static calculate()).
+// Re-enable once the simulation UI service layer is wired up.
 
+// import { PowerGenerationResolver } from '../resolvers/PowerGenerationResolver';
+// import { CityDemandResolver } from '../resolvers/CityDemandResolver';
+// import { EnergyStorageResolver } from '../resolvers/EnergyStorageResolver';
+// import type {
+//   SimulationData,
+//   SimulationInput,
+// } from '../types/SimulationTypes';
 
+// export type { SimulationData, SimulationInput };
 
-import { PowerGenerationResolver } from '../resolvers/PowerGenerationResolver';
-import { CityDemandResolver } from '../resolvers/CityDemandResolver';
-import { EnergyStorageResolver } from '../resolvers/EnergyStorageResolver';
-import type {
-  SimulationData,
-  SimulationInput,
-} from '../types/SimulationTypes';
+// export class SimulationUIService {
+//   private readonly powerResolver: PowerGenerationResolver;
+//   private readonly demandResolver: CityDemandResolver;
+//   private readonly storageResolver: EnergyStorageResolver;
 
-// Re-export types for external consumption
-export type { SimulationData, SimulationInput };
+//   constructor(
+//     powerResolver = new PowerGenerationResolver(),
+//     demandResolver = new CityDemandResolver(),
+//     storageResolver = new EnergyStorageResolver(),
+//   ) {
+//     this.powerResolver = powerResolver;
+//     this.demandResolver = demandResolver;
+//     this.storageResolver = storageResolver;
+//   }
 
-/**
- * Orchestrates the simulation-related resolvers and returns a single
- * SimulationData object containing generation, demand and storage series.
- */
-export class SimulationUIService {
-  private readonly powerResolver: PowerGenerationResolver;
-  private readonly demandResolver: CityDemandResolver;
-  private readonly storageResolver: EnergyStorageResolver;
-
-  constructor(
-    powerResolver = new PowerGenerationResolver(),
-    demandResolver = new CityDemandResolver(),
-    storageResolver = new EnergyStorageResolver(),
-  ) {
-    this.powerResolver = powerResolver;
-    this.demandResolver = demandResolver;
-    this.storageResolver = storageResolver;
-  }
-
-  public getSimulationData(input: SimulationInput): SimulationData {
-    const generation = this.powerResolver.resolve(input);
-    const demand = this.demandResolver.resolve(input);
-    const storage = this.storageResolver.resolve(
-      input,
-      generation,
-      demand,
-    );
-
-    return {
-      generation,
-      demand,
-      storage,
-    };
-  }
-}
+//   public getSimulationData(input: SimulationInput): SimulationData {
+//     const generation = this.powerResolver.resolve(input);
+//     const demand = this.demandResolver.resolve(input);
+//     const storage = this.storageResolver.resolve(input, generation, demand);
+//     return { generation, demand, storage };
+//   }
+// }
